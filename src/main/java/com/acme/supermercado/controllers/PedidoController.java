5fe8b44583636ad20b5cb2b58e5039725ad750ad
CarlosGoncalves <carlos_ojr@live.com>
2023-09-25 18:54:10 -0300

    Criação parcial da estrutura Pedido, responsavel por resumir e finalizar a compra.

--- src/main/java/com/acme/supermercado/controllers/PedidoController.java
+++ src/main/java/com/acme/supermercado/controllers/PedidoController.java
@@ -2,6 +2,8 @@ package com.acme.supermercado.controllers;
 
 import com.acme.supermercado.dtos.TotalDTO;
 import com.acme.supermercado.services.interfaces.PedidoInterface;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,9 @@ public class PedidoController {
         this.pedidoService = pedidoService;
     }
 
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Retorna os produtos, quantidades, valores e total da compra com sucesso"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<TotalDTO> totalizarPedido(@PathVariable Long id) {
         return ResponseEntity.ok(pedidoService.resumoPedido(id));
